fc0ea25a82687472171945259cdd4a07f548e302
LucasMirel <Icannaydien@gmail.com>
2026-09-24 11:46:50 +0700

    y

--- tspasha/new.js
+++ tspasha/new.js
@@ -1,16 +1,12 @@
 "use strict";
-const nama = "Ihsan Dari kelas 10";
 const studentName = document.getElementById('name');
 const studentClass = document.getElementById('class');
+const output = document.getElementById("output");
 function display() {
-    alert('This is display function');
-}
-function display2() {
-    alert('Now that you know you dont have to press the other one ');
+    if (output) {
+        output.innerHTML = `My Name is ${studentName.value} from class ${studentClass.value}`;
+    }
 }
 const button = document.getElementById("btn");
 button.addEventListener('click', display);
-const output = document.getElementById("output");
-if (output) {
-    output.innerHTML = `halo, ${nama}`;
-}
+
--- tspasha/new.ts
+++ tspasha/new.ts
@@ -1,13 +1,14 @@
-const nama : string = "Ihsan Dari kelas 10"
-const studentName = document.getElementById('name') as HTMLInputElement;
-const studentClass = document.getElementById('class') as HTMLInputElement;
+const studentName = document.getElementById('name') as 
+HTMLInputElement;
+const studentClass = document.getElementById('class') as 
+HTMLInputElement;
+const output = document.getElementById("output") as
+HTMLElement;
 
-function display() {
-    alert('This is display function')
-}
-
-function display2() {
-    alert('Now that you know you dont have to press the other one ')
+function display(): void {
+    if(output) {
+        output.innerHTML = `My Name is ${studentName.value} from class ${studentClass.value}`;
+    }
 }
 
 
@@ -15,8 +16,6 @@ const output = document.getElementById("btn") as
 HTMLButtonElement;
 button.addEventListener('click', display)
 
-const output = document.getElementById("output");
-
-if(output) {
-    output.innerHTML = `halo, ${nama}`;
+if (output) {
+    output.inn
 }
