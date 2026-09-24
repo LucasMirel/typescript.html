f1a434fcc2426880f69dde142758c6af4ae971b8
LucasMirel <Icannaydien@gmail.com>
2026-09-24 11:27:30 +0700

    use

--- tspasha/new.js
+++ tspasha/new.js
@@ -1,8 +1,15 @@
 "use strict";
-const nama = "Ihsan";
+const nama = "Ihsan Dari kelas 10";
+const studentName = document.getElementById('name');
+const studentClass = document.getElementById('class');
 function display() {
     alert('This is display function');
 }
+function display2() {
+    alert('Now that you know you dont have to press the other one ');
+}
+const button = document.getElementById("btn");
+button.addEventListener('click', display);
 const output = document.getElementById("output");
 if (output) {
     output.innerHTML = `halo, ${nama}`;
--- tspasha/new.ts
+++ tspasha/new.ts
@@ -1,8 +1,19 @@
-const nama : string = "Ihsan";
+const nama : string = "Ihsan Dari kelas 10"
+const studentName = document.getElementById('name') as HTMLInputElement;
+const studentClass = document.getElementById('class') as HTMLInputElement;
 
 function display() {
     alert('This is display function')
 }
+
+function display2() {
+    alert('Now that you know you dont have to press the other one ')
+}
+
+
+const output = document.getElementById("btn") as 
+HTMLButtonElement;
+button.addEventListener('click', display)
 
 const output = document.getElementById("output");
 
